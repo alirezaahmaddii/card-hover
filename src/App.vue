fc452b233c58017ed77d2e39ac460bbd2a62765f
alirezaahmadi1308 <alirezaahmadi1308@gmail.com>
2022-06-29 16:50:49 +0430

    card-hover

---
+++ src/App.vue
@@ -0,0 +1,366 @@
+<template>
+  <header class=" header show-on-scroll">
+    <section class=" comment  ">
+      <div class="container">
+        <div class="d-flex justify-content-between">
+          <div class="comment__box box__4 d-flex flex-column justify-content-center align-items-center ">
+            <figcaption>
+              <div class="fig-text d-flex flex-column w-100 h-100">
+                <div class="text-comment-box d-flex justify-content-between">
+                  <p class="fw-bold px-4">comment</p>
+                  <span class="px-4">...</span>
+                </div>
+                <div class="d-flex flex-column">
+                  <span class="px-4 pt-2">Text</span>
+                  <div class="d-flex pt-2 px-4 fig-img">
+                    <figure>
+                      <img src="/media/image/user.jpg" alt="" width="44px" height="44px">
+                    </figure>
+                    <p class="d-flex flex-column px-2">
+                      <span style="font-size: 13px">Louis Ringal</span>
+                      <span style="opacity: 0.3;font-size: 12px">2 hours ago</span>
+                    </p>
+
+                  </div>
+                  <span class="px-4" style="font-size: 12px;padding-bottom: 3rem">Let's swap out the image
+                  here for the other one we have</span>
+                </div>
+                <div class="d-flex justify-content-between px-4 reply">
+                  <p>7 reply</p>
+
+                </div>
+              </div>
+            </figcaption>
+            <div class="comment__image">
+              <div class="image__border">
+
+              </div>
+              <figure>
+                <img src="/media/image/user.jpg" alt="user">
+              </figure>
+            </div>
+            <div class="d-flex flex-column text-center pt-4">
+              <span>Jack Ringel</span>
+              <span>Jackringel@gmail.com</span>
+            </div>
+          </div>
+          <div class="comment__box box__4 d-flex flex-column justify-content-center align-items-center ">
+            <figcaption>
+              <div class="fig-text d-flex flex-column w-100 h-100">
+                <div class="text-comment-box d-flex justify-content-between">
+                  <p class="fw-bold px-4">comment</p>
+                  <span class="px-4">...</span>
+                </div>
+                <div class="d-flex flex-column">
+                  <span class="px-4 pt-2">Text</span>
+                  <div class="d-flex pt-2 px-4 fig-img">
+                    <figure>
+                      <img src="/media/image/user.jpg" alt="" width="44px" height="44px">
+                    </figure>
+                    <p class="d-flex flex-column px-2">
+                      <span style="font-size: 13px">Louis Ringal</span>
+                      <span style="opacity: 0.3;font-size: 12px">2 hours ago</span>
+                    </p>
+
+                  </div>
+                  <span class="px-4" style="font-size: 12px;padding-bottom: 3rem">Let's swap out the image
+                  here for the other one we have</span>
+                </div>
+                <div class="d-flex justify-content-between px-4 reply">
+                  <p>7 reply</p>
+
+                </div>
+              </div>
+            </figcaption>
+            <div class="comment__image">
+              <div class="image__border">
+
+              </div>
+              <figure>
+                <img src="/media/image/user.jpg" alt="user">
+              </figure>
+            </div>
+            <div class="d-flex flex-column text-center pt-4">
+              <span>Jack Ringel</span>
+              <span>Jackringel@gmail.com</span>
+            </div>
+          </div>
+          <div class="comment__box box__4 d-flex flex-column justify-content-center align-items-center ">
+            <figcaption>
+              <div class="fig-text d-flex flex-column w-100 h-100">
+                <div class="text-comment-box d-flex justify-content-between">
+                  <p class="fw-bold px-4">comment</p>
+                  <span class="px-4">...</span>
+                </div>
+                <div class="d-flex flex-column">
+                  <span class="px-4 pt-2">Text</span>
+                  <div class="d-flex pt-2 px-4 fig-img">
+                    <figure>
+                      <img src="/media/image/user.jpg" alt="" width="44px" height="44px">
+                    </figure>
+                    <p class="d-flex flex-column px-2">
+                      <span style="font-size: 13px">Louis Ringal</span>
+                      <span style="opacity: 0.3;font-size: 12px">2 hours ago</span>
+                    </p>
+
+                  </div>
+                  <span class="px-4" style="font-size: 12px;padding-bottom: 3rem">Let's swap out the image
+                  here for the other one we have</span>
+                </div>
+                <div class="d-flex justify-content-between px-4 reply">
+                  <p>7 reply</p>
+
+                </div>
+              </div>
+            </figcaption>
+            <div class="comment__image">
+              <div class="image__border">
+
+              </div>
+              <figure>
+                <img src="/media/image/user.jpg" alt="user">
+              </figure>
+            </div>
+            <div class="d-flex flex-column text-center pt-4">
+              <span>Jack Ringel</span>
+              <span>Jackringel@gmail.com</span>
+            </div>
+          </div>
+          <div class="comment__box box__4 d-flex flex-column justify-content-center align-items-center ">
+            <figcaption>
+              <div class="fig-text d-flex flex-column w-100 h-100">
+                <div class="text-comment-box d-flex justify-content-between">
+                  <p class="fw-bold px-4">comment</p>
+                  <span class="px-4">...</span>
+                </div>
+                <div class="d-flex flex-column">
+                  <span class="px-4 pt-2">Text</span>
+                  <div class="d-flex pt-2 px-4 fig-img">
+                    <figure>
+                      <img src="/media/image/user.jpg" alt="" width="44px" height="44px">
+                    </figure>
+                    <p class="d-flex flex-column px-2">
+                      <span style="font-size: 13px">Louis Ringal</span>
+                      <span style="opacity: 0.3;font-size: 12px">2 hours ago</span>
+                    </p>
+
+                  </div>
+                  <span class="px-4" style="font-size: 12px;padding-bottom: 3rem">Let's swap out the image
+                  here for the other one we have</span>
+                </div>
+                <div class="d-flex justify-content-between px-4 reply">
+                  <p>7 reply</p>
+
+                </div>
+              </div>
+            </figcaption>
+            <div class="comment__image">
+              <div class="image__border">
+
+              </div>
+              <figure>
+                <img src="/media/image/user.jpg" alt="user">
+              </figure>
+            </div>
+            <div class="d-flex flex-column text-center pt-4">
+              <span>Jack Ringel</span>
+              <span>Jackringel@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </header>
+
+
+
+
+</template>
+<script>
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger.js';
+import 'gsap/ScrollToPlugin';
+
+export default {
+  data: () => ({
+
+  }),
+  methods: {
+
+  },
+  mounted() {
+
+  },
+}
+</script>
+<style lang="scss">
+*, *:before, *:after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+  border: 0 none;
+  position: relative;
+}
+
+
+
+.comment {
+  padding-bottom: 5rem;
+  padding-top: 22rem;
+
+  .comment__box {
+    width: 18%;
+    border: 1px solid #d4d4d4;
+    align-items: center;
+    padding: 2rem 0;
+    transition: 2s all ease-in-out;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+
+    &:hover {
+      figcaption {
+        transform: translateY(0px);
+        position: absolute;
+        z-index: 99;
+        opacity: 1;
+      }
+
+      &:hover .image__border {
+        transform: rotate(206deg);
+        z-index: 0;
+      }
+    }
+
+    .comment__image {
+      z-index: 1;
+
+      .image__border {
+        border: 1px solid #1D15FF;
+        border-radius: 100%;
+        position: absolute;
+        z-index: 99 !important;
+        width: 161px;
+        height: 161px;
+        transition: 0.9s all ease-in-out;
+
+        &::before {
+          content: "";
+          width: 18px;
+          height: 18px;
+          position: absolute;
+          border: 2px solid #ffffff;
+          border-radius: 100%;
+          background-color: #1D15FF;
+          right: 27px;
+          top: 6px;
+          z-index: 99;
+        }
+      }
+
+      figure {
+        border-radius: 100%;
+        margin: 0;
+      }
+
+      img {
+        border-radius: 100%;
+        padding: 6px;
+      }
+    }
+  }
+
+  .box__4 {
+    figcaption {
+      left: 0;
+      transform: translateX(-900px);
+      max-width: 100%;
+      width: 100%;
+      opacity: 0;
+
+    }
+  }
+
+  figcaption {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    background: #ffffff;
+    transition: 1.5s all ease;
+    max-width: 0;
+    width: 0;
+    opacity: 1;
+    z-index: 99;
+  }
+
+  .fig-text {
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+
+  .text-comment-box {
+    position: relative;
+
+    &::after {
+      content: '';
+      width: 100%;
+      height: 1px;
+      background-color: #d4d4d4;
+      position: absolute;
+      bottom: 5px;
+
+
+    }
+  }
+
+  .fig-img {
+    border-radius: 100%;
+
+    img {
+      border: 1px solid #1D15FF;
+      border-radius: 100%;
+      padding: 2px;
+
+    }
+  }
+
+  .reply {
+    position: relative;
+
+    p {
+      color: #0074FF;
+    }
+
+    @keyframes arrow {
+      0% {
+        transform: translateX(0px);
+      }
+      50% {
+        transform: translateX(2px);
+      }
+      100% {
+        transform: translateX(0px);
+      }
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 25px;
+      height: 25px;
+      right: 20px;
+      background-image: url("./public/media/svg/arrow-right-dark.svg");
+      animation: arrow 1s infinite;
+    }
+  }
+
+
+}
+
+
+
+
+
+</style>
